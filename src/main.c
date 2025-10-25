2b5e8fba7cace9724f1184a9d47aa8ab9f27e1f3
rexept <io@etriadi.com>
2025-10-25 00:45:10 -0700

    refactor(cli): flag parsing

--- src/main.c
+++ src/main.c
@@ -26,10 +26,7 @@ int main(int argc, char* argv[]) {
             show_artist = true;
         } else if (strcmp(argv[i], "--show-title") == 0) {
             show_title = true;
-        } else if (strcmp(argv[i], "--help") == 0) {
-            print_help();
-            return 0;
-        } else if (strcmp(argv[i], "-h") == 0) {
+        } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
             print_help();
             return 0;
         }
